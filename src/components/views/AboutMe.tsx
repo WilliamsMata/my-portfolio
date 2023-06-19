bff4160b8e0889a9b3b1abe0f361bc42c0dfd3c9
Williams Mata <williams.rm99@gmail.com>
2023-06-19 16:48:44 -0400

    ring purple

--- src/components/views/AboutMe.tsx
+++ src/components/views/AboutMe.tsx
@@ -87,6 +87,7 @@ export const AboutMe: FC<Props> = (props) => {
                   placeholder="Your name"
                   name="Name"
                   required
+                  className="focus-visible:ring-purple-700"
                 />
               </div>
 
@@ -98,6 +99,7 @@ export const AboutMe: FC<Props> = (props) => {
                   id="email"
                   name="Email"
                   placeholder="Your email"
+                  className="focus-visible:ring-purple-700"
                 />
               </div>
 
@@ -108,11 +110,11 @@ export const AboutMe: FC<Props> = (props) => {
                     placeholder="Type your message here."
                     id="message"
                     name="message"
-                    className="resize-y overflow-auto"
+                    className="resize-y overflow-auto focus-visible:ring-purple-700"
                   />
                 </div>
                 <Button
-                  className="aspect-square rounded-full p-2 transition-all active:scale-90 active:bg-violet-600"
+                  className="aspect-square rounded-full p-2 transition-all focus-visible:ring-purple-600 active:scale-90 active:bg-violet-600"
                   variant="ghost"
                   type="submit"
                   title="Send message"
