519f971fe732a31c830d9f9e11a6b9db38fe2adc
Williams Mata <williams.rm99@gmail.com>
2023-06-24 15:08:31 -0400

    link rel icon darkmode and light mode

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,18 +1,8 @@
-import { type PropsWithChildren, type FC, useState, useEffect } from "react";
+import type { PropsWithChildren, FC } from "react";
 import Head from "next/head";
 import { Footer, Navbar, Sidebar } from "~/components";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
-  const [isSchemaDark, setIsSchemaDark] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setIsSchemaDark(
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      );
-    }
-  }, []);
-
   return (
     <>
       <Head>
@@ -30,7 +20,13 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
 
         <link
           rel="icon"
-          href={`/icons/wm-${isSchemaDark ? "dark" : "light"}.svg`}
+          media="(prefers-color-scheme: dark)"
+          href="/icons/wm-dark.svg"
+        />
+        <link
+          rel="icon"
+          media="(prefers-color-scheme: light)"
+          href="/icons/wm-light.svg"
         />
       </Head>
 
