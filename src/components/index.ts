63aa272330db97ae23ebeb4f5e6753eb8f1f311a
Williams Mata <williams.rm99@gmail.com>
2023-06-26 18:53:09 -0400

    theme provider component removed

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,5 @@ export * from "./Footer";
 export * from "./Navbar";
 export * from "./Sidebar";
 export * from "./icons";
-export * from "./theme-provider";
 export * from "./ui";
 export * from "./views";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { Roboto_Mono } from "next/font/google";
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 import { UIProvider } from "~/context/ui";
-import { ThemeProvider } from "~/components";
+import { ThemeProvider } from "next-themes";
 
 const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
